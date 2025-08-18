89fa03d532ce662363cb2d42de17f8542989d3d4
mahawami55 <121563274+mahawami55@users.noreply.github.com>
2025-08-18 13:58:11 +0200

    LoginScreen.jsx

--- LoginScreen.jsx
+++ LoginScreen.jsx
@@ -1,3 +1,4 @@
+{"status":500,"name":"Error","message":"Input buffer contains unsupported image format"}
 import React, { useState, useEffect } from 'react';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
@@ -243,6 +244,7 @@ const LoginScreen = ({ onLogin }) => {
 };
 
 export default LoginScreen;
+
 
 
 
